b296d667508a455f44e625099855e81cb6b0f5e3
brianwilson3 <brianwilson2@users.noreply.github.com>
2021-12-04 16:05:32 +0000

    Initial commit

---
+++ imcomfunc.py
@@ -0,0 +1,33 @@
+def getfiles(newpath):
+    #push button up again
+    #newpath=path+"/"+dir
+    print ("selected path",newpath)
+    #filelist=[]
+    if not os.listdir(newpath):
+        print(f'Try another - that folder {dir} is empty')#messagebox should pop up to display this
+        return
+    else:
+        files=os.listdir(newpath)
+        for file in files:
+            #print(file,'in files')
+            filetype(newpath,file)
+            #filelist.append(file)
+    cv.destroyAllWindows()
+    return
+
+def diff(image1,image2):
+    difference= cv2.subtract(image1,image2)
+    #print("Difference equals ",np.sum(difference))
+    result=not np.any(difference) # differewnce is all zeros it will return false
+    w,h,c=difference.shape
+    total_pixel_value_count=w*h*c*255  # Find the silimarity between this images %
+    percentage_match=(total_pixel_value_count -np.sum(difference))/total_pixel_value_count*100
+    #print(percentage_match)
+    if result is True:
+        print (f"the images ("+image1+" and "+image2+") are {percentage_match} the same")
+    else:
+        res="result"+str(i)+".jpg"
+        #cv2.imwrite(res, difference)
+        print (f"The images ("+image1+" and "+image2+") are {percentage_match} different")
+        i=i+1
+    return
---
+++ imcompmain.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+import numpy as np
+from imcompfunc import getfiles, diff
+
+q=input("Search for duplicates in '1' folder, or search for copies in '2' folders - enter '1' or '2'")
+
+pa="c:/Users/brian/Pictures/sdcopy"
+
+#compare a folder for duplicates
+#folder1=input("enter folder1")
+
+#compare the images within folders 'Brora' and 'Brora2'
+folder1=input("enter folder1")
+folder2=input("enter folder2")
+
+print ("checking "+folder1+" and "+folder2+" for differences")
+#getfiles
